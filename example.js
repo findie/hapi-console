24bb87589574d5bff2d6b4ca9b5afe75d265a245
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2018-03-30 18:59:59 +0300

    Update: Breaking! Moved support from hapi 16.x to 17.x Update: Node requirements now at >=7.6.0

--- example.js
+++ example.js
@@ -1,69 +1,65 @@
 const Hapi = require('hapi');
 
-const server = new Hapi.Server({ debug: false });
+const server = new Hapi.Server({ port: 3000 });
 
 const plugin = require('./index');
 
-server.connection({ port: 8080, labels: ['test', '1'] });
-server.connection({ port: 8081, labels: ['test', '2'] });
+// server.connection({ port: 8080, labels: ['test', '1'] });
+// server.connection({ port: 8081, labels: ['test', '2'] });
 // server.connection({ port: 8081 });
 
 server.route({
-    path: '/',
-    method: 'get',
-    handler: (req, res) => {
-        'use strict';
-        req.log('error', 'err');
-        req.log('error', new Error());
-        req.log('log', 'log 1');
-        setTimeout(() => req.log('later', 'log 2'), 60);
-        setTimeout(() => res(''), 200);
-        server.log('log', 'this is a server log');
-        server.log(['log', 'test']);
-        req.log(['log', 'test']);
-        setTimeout(() => {
-            throw new Error('up')
-        }, 100);
-    }
+  path: '/',
+  method: 'get',
+  handler: (req) => {
+    'use strict';
+    req.log('error', 'err');
+    req.log('error', new Error());
+    req.log('log', 'log 1');
+    setTimeout(() => req.log('later', 'log 2'), 60);
+    server.log('log', 'this is a server log');
+    server.log(['log', 'test']);
+    req.log(['log', 'test']);
+    throw new Error('up');
+
+    return '';
+  }
 });
 server.route({
-    path: '/ignore/me',
-    method: 'get',
-    handler: (req, res)=> {
-        'use strict';
-        return res('');
-    }
+  path: '/ignore/me',
+  method: 'get',
+  handler: (req, res) => {
+    'use strict';
+    return res('');
+  }
 });
 
 
 server.route({
-    path: '/test',
-    method: 'get',
-    handler: (req, res)=> {
-        'use strict';
-        return res('');
-    }
+  path: '/test',
+  method: 'get',
+  handler: (req) => {
+    'use strict';
+    return '';
+  }
 });
 
-server.register({
-    register: plugin,
+(async () => {
+  await server.register({
+    plugin,
     options: {
-        ignore: ['/ignore/me'],
-        custom: {
-            'auth.credentials.uid': true,
-            'query': true,
-            'headers': (x) => JSON.stringify(x).replace(/"/g, '')
-        },
-        customFullLengthKey: false,
-        userFilter: {
-
-        }
+      ignore: ['/ignore/me'],
+      custom: {
+        'auth.credentials.uid': true,
+        'query': true,
+        'headers': (x) => JSON.stringify(x).replace(/"/g, '')
+      },
+      customFullLengthKey: false,
+      userFilter: {}
     }
-}, (err) => {
+  });
 
-    if (err) {
-        throw err;
-    }
-});
+  await server.start();
+})().catch(console.error);
 
-server.start();
+
